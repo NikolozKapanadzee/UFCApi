35386dcdf1c520319b20bd6b791722cb9b009817
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-12-10 20:39:21 +0400

    feat: configurated mongo base

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FightersModule } from './fighters/fighters.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FightersModule],
+  imports: [
+    FightersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URL!),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
