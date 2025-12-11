e154941cce8a51b4b2173d47d8edcaf2273c2395
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-12-11 21:41:00 +0400

    feat: finished fighters CRUD

--- src/fighters/fighters.service.ts
+++ src/fighters/fighters.service.ts
@@ -1,30 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateFighterDto } from './dto/create-fighter.dto';
 import { UpdateFighterDto } from './dto/update-fighter.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Fighter } from './schema/fighter.schema';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 
 @Injectable()
 export class FightersService {
   constructor(
     @InjectModel(Fighter.name) private fighterModel: Model<Fighter>,
   ) {}
-  create(createFighterDto: CreateFighterDto) {}
-
-  findAll() {
-    return `This action returns all fighters`;
+  async create(createFighterDto: CreateFighterDto) {
+    const newFighter = await this.fighterModel.create(createFighterDto);
+    return {
+      message: 'fighter created successfully',
+      data: newFighter,
+    };
+  }
+  async findAll() {
+    const fighters = await this.fighterModel.find();
+    return fighters;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} fighter`;
+  async findOne(id: string) {
+    if (!isValidObjectId(id)) {
+      throw new BadRequestException('Invalid ID format');
+    }
+    const fighter = await this.fighterModel.findById(id);
+    if (!fighter) {
+      throw new NotFoundException('fighter can not be found');
+    }
+    return fighter;
   }
 
-  update(id: number, updateFighterDto: UpdateFighterDto) {
-    return `This action updates a #${id} fighter`;
+  async update(id: string, updateFighterDto: UpdateFighterDto) {
+    if (!isValidObjectId(id)) {
+      throw new BadRequestException('Invalid ID format');
+    }
+    const updatedFighter = await this.fighterModel.findByIdAndUpdate(
+      id,
+      updateFighterDto,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+    if (!updatedFighter) {
+      throw new NotFoundException('fighter can not be found');
+    }
+    return {
+      message: 'fighter updated successfully',
+      data: updatedFighter,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} fighter`;
+  async remove(id: string) {
+    if (!isValidObjectId(id)) {
+      throw new BadRequestException('Invalid ID format');
+    }
+    const deletedFighter = await this.fighterModel.findByIdAndDelete(id);
+    if (!deletedFighter) {
+      throw new NotFoundException('fighter can not be found');
+    }
+    return {
+      message: 'fighter deleted successfully',
+      data: deletedFighter,
+    };
   }
 }
