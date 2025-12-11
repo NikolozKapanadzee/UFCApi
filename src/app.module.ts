2584dba5b6f953b324780baf197a58b467a34e6e
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-12-11 23:43:41 +0400

    feat: created fight folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { FightersModule } from './fighters/fighters.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { FightModule } from './fight/fight.module';
 
 @Module({
   imports: [
     FightersModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL!),
+    FightModule,
   ],
   controllers: [AppController],
   providers: [AppService],
